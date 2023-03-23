fdc155cfff7b69a3766b3d18e8c84c50aa3574d1
furaga <furaga.fukahori@gmail.com>
2023-03-23 18:45:41 +0900

    sample to take screenshot

--- hello_obs_websocket.py
+++ hello_obs_websocket.py
@@ -20,6 +20,23 @@ ws.connect()
 #print(ws.call(requests.GetSourceSettings()))  # "from-python", "text"))
 import time
 
+for i in range(10):
+    ws.call(
+        requests.TakeSourceScreenshot(
+            # sourceName="映像キャプチャデバイス",
+            sourceName="映像キャプチャデバイス",
+            embedPictureFormat="jpg",
+            saveToFilePath=r"C:\Users\furag\Documents\prog\python\Life_of_Zundamon\out.jpg",
+            width = 1920,
+            height = 1080,
+            compressionQuality = 100,
+        )
+    )
+        # "imageWidth":1920,"imageHeight":1080,"imageCompressionQuality":100})
+    time.sleep(3)
+ws.disconnect()
+exit()
+
 obsTextChange("from-python", "HELLO OBS WEBSOCKET")
 time.sleep(1.0)
 obsTextChange("from-python", "HELLO")
